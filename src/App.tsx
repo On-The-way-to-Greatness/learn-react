92701a23c5af02b9c7c90b039a9309cafbfd58d3
biniyam-29 <bnmmarkos@gmail.com>
2025-07-29 10:44:51 +0300

    feat: built weather app using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { useEffect, useState } from "react";
 
+const url =
+  "http://api.weatherapi.com/v1/current.json?key=fd9de28b21e84760bb061639252907&q=London";
+
 function App() {
   const [temp, setTemp] = useState(0);
+  useEffect(() =>
+    {
+      const fetchData = async () => {
+        const result = await fetch(url);
+        result.json().then(json => {
+          setTemp(json.current.temp_f)
+        });
+      }
+      fetchData();
+    },
+    []
+  );
 
-  return <div>The current temp value is {temp}F.</div>;
+  return <div>The current temp value is {temp} F.</div>;
 }
 
 export default App;
